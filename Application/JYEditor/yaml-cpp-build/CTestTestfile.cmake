899f8d468937835fa4ca5e969fa658b4afb9195c
gadget114514 <gadget114514@gmail.com>
2026-05-24 10:58:11 +0900

    arrange messy files

---
+++ Application/JYEditor/yaml-cpp-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/vcpkg/buildtrees/yaml-cpp/src/-cpp-0.9.0-41889444fe.clean
+# Build directory: D:/ws/Ecode/Application/JYEditor/yaml-cpp-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
